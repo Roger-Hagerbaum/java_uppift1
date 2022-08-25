bf5341be0cd55460f412cee684a7cb34a6efb131
Roger Hagerbaum <roger.hagerbaum@lerniastudent.se>
2022-08-25 13:22:57 +0200

    Completed

--- src/uppgift1.java
+++ src/uppgift1.java
@@ -2,7 +2,21 @@ public class uppgift1 {
 
 
     public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            if (i == 42) {
+                System.out.println("Answer to the Ultimate Question of Life, the Universe, and Everything");
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Fizzbuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
 
-
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else
+                System.out.println(i);
+            i++;
+        }
     }
 }
+
